b06d61c3a1d8948685f77ae5dd85ffd33804776c
Tsoiffer <tsoiffer@frba.utn.edu.ar>
2023-03-27 15:43:26 -0300

    se carga ejercicio 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,7 +11,21 @@ const totalLikes = (blogs) => {
     : blogs.map((blog) => blog.likes).reduce(reducer, 0);
 };
 
+const mostBlogs = (blogs) => {
+  const blogsForAthor = blogs.map((blog) => {
+    const totalBlogs = blogs.filter(
+      (otherBlog) => otherBlog.author === blog.author
+    ).length;
+    return { author: blog.author, blogs: totalBlogs };
+  });
+  const max = blogsForAthor.reduce(function (prev, current) {
+    return prev.blogs > current.blogs ? prev : current;
+  });
+  return max;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  mostBlogs,
 };
